fa50c782d49ef5554bcb8b1eb61346e8db715cd1
NataliReise <info@glossai.de>
2026-06-28 13:48:11 +0200

    Add First Spark walkthrough text

---
+++ modules/nexus_01_nexus_mesomerie/first_spark/first_spark/guidance.py
@@ -0,0 +1,15 @@
+"""Guidance texts for Nexus 0.1 - First Spark."""
+
+from __future__ import annotations
+
+
+WALKTHROUGH_TEXT = """Spoiler warning:
+This walkthrough reveals the complete First Spark solution.
+
+Complete path:
+  look
+  read welcome.log
+  read spark.note
+  link spark
+  unlock
+"""
