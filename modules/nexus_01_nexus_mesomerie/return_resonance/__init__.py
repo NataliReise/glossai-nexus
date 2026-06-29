41a7d36d1e89e29d27a5b0f98670614c5372a5ca
NataliReise <info@glossai.de>
2026-06-29 23:56:33 +0200

    code: export return resonance result helpers

--- modules/nexus_01_nexus_mesomerie/return_resonance/__init__.py
+++ modules/nexus_01_nexus_mesomerie/return_resonance/__init__.py
@@ -7,15 +7,20 @@ First Spark core.
 from .artifact import ReturnArtifact, ReturnArtifactParseError, parse_return_artifact
 from .slots import ReturnSlot, ReturnSlotState, load_return_slots
 from .matching import MatchResult, MatchStatus, match_return_artifact
+from .result import ReturnResult, ReturnResultError, compose_return_result, open_return_result
 
 __all__ = [
     "MatchResult",
     "MatchStatus",
     "ReturnArtifact",
     "ReturnArtifactParseError",
+    "ReturnResult",
+    "ReturnResultError",
     "ReturnSlot",
     "ReturnSlotState",
+    "compose_return_result",
     "load_return_slots",
     "match_return_artifact",
+    "open_return_result",
     "parse_return_artifact",
 ]
